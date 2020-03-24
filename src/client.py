e6b3f627ec09716e16d6200e254c235305d05347
Pavel Shipilov <splkmaxdk@gmail.com>
2020-03-24 13:11:15 +0300

    Fix client

--- src/client.py
+++ src/client.py
@@ -1,6 +1,5 @@
-import http
-import json
-import urllib.parse
+#!/usr/bin/env python3
+
 import http.client as http_client
 
 
@@ -40,43 +39,48 @@ def input_number(n, fun):
     return tuple(ret)
 
 
-
-def run(host, port, method=None, path=None, body=None):
+def run(host, port, method=None, arg1=None, arg2=None):
     conn = http_client.HTTPConnection(host, port)
+    print(str.format('The client has bean connected to {0}:{1}', host, port))
 
-    if method is not None and path is not None:
-        if method != 'GET' or method != 'POST':
+    if method is not None:
+        if method == 'GET':
+            conn.request('GET', str.format('/visited_domains?from={0}&to={1}', arg1, arg2))
+        elif method == 'POST':
+            conn.request('POST', '/visited_links', arg1, {
+                'Content-type': 'application/json'
+            })
+        else:
             print('Error: Method should be "GET" or "POST"')
             return
-        conn.request(method, path)
+
         status, resp = read_responce(conn)
         print_responce(status, resp)
         return
 
     while True:
-        print('Please, enter 1 for get inforamtion abount visites or 2 for do visites')
+        print('Please, enter 1 for a GET request or 2 for a POST request')
         mod = input_number(1,
             lambda x: 'Error: Please enter 1 or 2!' if x != 1 and x != 2 else None
         )
         if mod is None:
             break
         elif mod == 1:
-            print('Please enter the start and end of time interval (unix time)')
+            print('Please, enter the start and end of the time interval (unix time)')
             _from, _to = input_number(2,
                 lambda x: 'Error: Value shoul be above zero!' if x < 0 else None
             )
             print(_from, _to)
             conn.request('GET', str.format('/visited_domains?from={0}&to={1}', _from, _to))
         elif mod == 2:
-            print('Please write body {"links": [...]}')
+            print('Please, enter a body {"links": [...]}')
             try:
                 body = input()
             except KeyboardInterrupt:
                 break
-            headers = {
+            conn.request('POST', '/visited_links', body, {
                 'Content-type': 'application/json'
-            }
-            conn.request('POST', '/visited_links', body, headers)
+            })
 
         status, resp = read_responce(conn)
         print_responce(status, resp)
@@ -88,8 +92,8 @@ if __name__ == '__main__':
     host = 'localhost'
     port = 8080
     method = None
-    path = None
-    body = None
+    arg1 = None
+    arg2 = None
     if len(argv) == 2:
         host = str(argv[1])
     if len(argv) >= 3:
@@ -98,12 +102,27 @@ if __name__ == '__main__':
         except ValueError:
             print('Error: Port should be a number!')
             exit(1)
-    if len(argv) >= 5:
+    if len(argv) >= 4:
         method = str(argv[3])
-        path = str(argv[4])
-    if len(argv) == 6:
-        body = str(argv[5])
+        if method == 'GET':
+            if len(argv) != 6:
+                print('A GET request should contains two args (are time)')
+                exit(1)
+            try:
+                arg1 = int(argv[4])
+                arg2 = int(argv[5])
+            except ValueError:
+                print('Error: Arguments should be a number!')
+                exit(1)
+        elif method == 'POST':
+            if len(argv) != 5:
+                print('A POST request should contains one arg (is body)')
+                exit(1)
+            arg1 = argv[4]
+        else:
+            print('Error: Method should be "GET" or "POST"')
+            exit(1)
 
-    run(host, port, method, path, body)
+    run(host, port, method, arg1, arg2)
 else:
     run('localhost', 8080)
